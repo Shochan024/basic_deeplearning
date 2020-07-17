2358ac9b6629614ccc6c1226fd9508297cfe7191
jockroiddev@gmail.com <jockroiddev@gmail.com>
2020-07-17 21:47:09 +0900

    xor multilayer perceptron

--- 01/perceptron/xor.py
+++ 01/perceptron/xor.py
@@ -1,18 +1,18 @@
 #!-*-coding:utf-8-*-
 import numpy as np
 
-def step( x , theta=-0.6 ):
+def step( x , theta=0 ):
     return np.where( x >= theta , 1 , 0 )
 
 
 def XOR( x_1 , x_2 ):
     X_1 = np.array([x_1,x_2])
-    W_1 = np.array([-0.5,-0.5])
-    u_1 = np.dot( W_1 , X_1 )
+    W_1 = np.array([[-0.2,-0.2],[0.1,0.1]])
+    b_1 = np.array([0.2,-0.1])
+    u_1 = np.dot( W_1 , X_1 ) + b_1
 
-    X_2 = step( x=u_1 )
-    print( X_2 )
-    W_2 = np.array([0,0.3,-0.3])
+    X_2 = step( x=u_1 , theta=0 ) #左側がNAND 右側がORのベクトルが出力される
+    W_2 = np.array([0.5,0.5])
     u_2 = np.dot( W_2 , X_2 )
 
     return u_2
@@ -20,9 +20,8 @@ def XOR( x_1 , x_2 ):
 def test( GATE ):
     for i in [0,1]:
         for j in [0,1]:
-            print( i,j )
-            GATE( i , j )
-            #print( "x_1={},x_2={} : y={}".format( i , j, step( x=GATE( i , j ) ) ) )
+            #print( i,j,GATE( i , j ) )
+            print( "x_1={},x_2={} : y={}".format( i , j, step( x=GATE( i , j ) , theta=0.8 ) ) )
             #print( "x_1={},x_2={} : u={}".format( i , j, GATE( i , j ) ) )
 
 test( GATE=XOR )
