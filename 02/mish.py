44128ebc3f0446db57916a8aa5b7c6a16001e43a
jockroiddev@gmail.com <jockroiddev@gmail.com>
2020-07-23 23:31:26 +0900

    softplus and sigmoid function defined

--- 02/mish.py
+++ 02/mish.py
@@ -2,15 +2,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def sigmoid( x ):
+    return 1 / ( 1 + np.exp( -x ) )
+
+def softplus( x ):
+    return np.log( 1 + np.exp( x ) )
+
 def mish( x ):
-    soft_plus = np.log( 1 + np.exp( x ) )
-    return x * np.tanh( soft_plus )
+    return x * np.tanh( softplus( x ) )
 
 def mish_prime( x ):
-    soft_plus = np.log( 1 + np.exp( x ) )
-    tanh = np.tanh( soft_plus )
-    sigmoid = 1 / ( 1 + np.exp( -x ) )
-    return x*sigmoid * ( 1 - tanh**2 ) + tanh
+    tanh = np.tanh( softplus( x ) )
+    return x*sigmoid(x) * ( 1 - tanh**2 ) + tanh
 
 x = np.arange( -5 , 5 , 0.1 )
 plt.grid()
